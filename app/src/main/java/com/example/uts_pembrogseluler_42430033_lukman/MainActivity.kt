7bdf3130cbf3f4d272496e137802c829c373dec1
lukmanresearch13-debug <lukmanresearch13@gmail.com>
2026-03-15 22:19:50 +0800

    Membuat logic untuk halaman login

--- app/src/main/java/com/example/uts_pembrogseluler_42430033_lukman/MainActivity.kt
+++ app/src/main/java/com/example/uts_pembrogseluler_42430033_lukman/MainActivity.kt
@@ -1,20 +1,31 @@
 package com.example.uts_pembrogseluler_42430033_lukman
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val etNamaDosen = findViewById<EditText>(R.id.etNamaDosen)
+        val btnMasuk = findViewById<Button>(R.id.btnMasuk)
+
+        btnMasuk.setOnClickListener {
+            val namaDosen = etNamaDosen.text.toString()
+
+            if (namaDosen.isNotEmpty()) {
+                // Modul 4: Intent untuk berpindah halaman dan membawa data
+                val intent = Intent(this, MainActivity2::class.java)
+                intent.putExtra("EXTRA_DOSEN", namaDosen)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Nama Dosen tidak boleh kosong!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
